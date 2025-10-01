73d03fc45c8b3f105b0ec15b963323d648c1f887
Yogesh_Chaudhari <133919650+Yogesh-1522@users.noreply.github.com>
2025-10-01 11:07:41 +0530

    Delete C-Programming/ASSIGNMENT/Assignment 9 directory

--- C-Programming/ASSIGNMENT/Assignment 9/10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int sub1=80, sub2=75, sub3=90, sub4=85, sub5=70;
-    int total = sub1 + sub2 + sub3 + sub4 + sub5;
-    float percentage = total / 5.0;
-    printf("Total Marks = %d\n", total);
-    printf("Percentage = %.2f%%\n", percentage);
-    return 0;
-}
