3328dc29a5d5e76a3484c1ae3398d803c796bac2
Yogesh_Chaudhari <133919650+Yogesh-1522@users.noreply.github.com>
2025-10-01 11:08:21 +0530

    Delete C-Programming/ASSIGNMENT/Assignment 0 directory

--- C-Programming/ASSIGNMENT/Assignment 0/4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int x = 5, y = 10, temp;
-    temp = x;
-    x = y;
-    y = temp;
-    printf("After Swapping: x = %d, y = %d\n", x, y);
-    return 0;
-}
--- C-Programming/ASSIGNMENT/Assignment 0/9.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-    float base = 10, height = 5, area;
-    area = 0.5 * base * height;
-    printf("Area of Triangle = %.2f\n", area);
-    return 0;
-}
